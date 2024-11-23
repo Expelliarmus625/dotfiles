69eb2315c38bc81b87f9284aa4f5834d79f80ea4
Ojas Barpande <ojas@Book.local>
2024-11-23 14:23:01 +0530

    initial setup nvim configuration

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("ojas.plugins-setup")
+require("ojas.core.options")
+require("ojas.core.keymaps")
+require("ojas.core.colorscheme")
+
+require("ojas.plugins.comment")
+require("ojas.plugins.nvim-tree")
+require("ojas.plugins.lualine")
+-- require("ojas.plugins.autopairs-config")
+require("ojas.plugins.telescope")
